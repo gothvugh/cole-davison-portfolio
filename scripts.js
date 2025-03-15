a4e9854af81b424974d25c7d28afe6b3555e7f4e
cole-davison <122580230+cole-davison@users.noreply.github.com>
2025-03-14 21:32:32 -0400

    Updated Portfolio

--- scripts.js
+++ scripts.js
@@ -1,7 +1,21 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.addEventListener("DOMContentLoaded", function () {
+const aboutMeBtn = document.getElementById("aboutMeBtn");
+const aboutMeSection = document.getElementById("aboutMeSection");
+const closeBtn = document.getElementById("closeBtn");
+
+// Open dropdown
+aboutMeBtn.addEventListener("click", function () {
+aboutMeSection.style.display = "flex";
+});
+
+// Close dropdown
+closeBtn.addEventListener("click", function () {
+aboutMeSection.style.display = "none";
+});
+
+// Close dropdown when clicking outside the content box
+aboutMeSection.addEventListener("click", function (event) {
+    if (event.target === aboutMeSection) {
+        aboutMeSection.style.display = "none";}
+    });
+});
